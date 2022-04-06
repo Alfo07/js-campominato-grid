7aded5385157b149775ea582576ae9343088bb6c
Alfo07 <ammiratialfonso07@gmail.com>
2022-04-06 17:39:31 +0200

    Struttura, html e css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//L'utente seleziona tramite l'apposito menù il livello di difficolta
+//Creare una funzione che genera una griglia, le dimensioni variano in baso al livello di difficoltà
+    //Con difficoltà Easy => tra 1 e 100
+    //Con difficoltà Hard => tra 1 e 81
+    //Con difficoltà Crazy => tra 1 e 49
+//Creare una funzione che permette di colorare i quadratini al click dell'utente
+
